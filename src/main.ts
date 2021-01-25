0fb180f414e4e1664e4e1de9324a4b928499c154
bbaktaeho <lth9036@gmail.com>
2021-01-25 15:27:37 +0900

    Add: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  SwaggerModule.setup('api', app, createDocument(app));
   await app.listen(3000);
 }
 bootstrap();
