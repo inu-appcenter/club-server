f3d1300c827d1723e85255bb37429561a0b61620
bbaktaeho <lth9036@gmail.com>
2021-03-05 17:21:32 +0900

    Add: 엔티티의 validate 메서드 추가, 예외와 타입 코드 추가

--- src/common/entity/Entity.ts
+++ src/common/entity/Entity.ts
@@ -1,8 +1,18 @@
+import { Code } from '../code/Code';
+import { Exception } from '../exception/Exception';
+import { Optional } from '../type/CommonTypes';
+import { ClassValidationDetails, ClassValidator } from '../utils/class-validator/ClassValidator';
+
 export class Entity {
   protected id: number;
 
   public getId(): number {
     if (typeof this.id === 'undefined') throw new Error(`${this.constructor.name}: ID is empty.`);
     return this.id;
+  }
+
+  public async validate(): Promise<void> {
+    const details: Optional<ClassValidationDetails> = await ClassValidator.validate(this);
+    if (details) throw Exception.new({ code: Code.ENTITY_VALIDATION_ERROR, data: details });
   }
 }
---
+++ src/common/utils/class-validator/ClassValidator.ts
@@ -0,0 +1,37 @@
+import { Optional } from '@/common/type/CommonTypes';
+import { validate, ValidationError } from 'class-validator';
+
+export type ClassValidationDetails = {
+  context: string;
+  errors: ClassValidationErrors[];
+};
+
+export type ClassValidationErrors = {
+  property: string;
+  message: string[];
+};
+
+export class ClassValidator {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  public static async validate<TTarget extends object>(
+    target: TTarget,
+    context?: string,
+  ): Promise<Optional<ClassValidationDetails>> {
+    let details: Optional<ClassValidationDetails>;
+    const errors: ValidationError[] = await validate(target);
+
+    if (errors.length > 0) {
+      details = {
+        context: context || target.constructor.name,
+        errors: [],
+      };
+      for (const error of errors) {
+        details.errors.push({
+          property: error.property,
+          message: error.constraints ? Object.values(error.constraints) : [],
+        });
+      }
+    }
+    return details;
+  }
+}
