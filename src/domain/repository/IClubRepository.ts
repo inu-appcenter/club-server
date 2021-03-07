591fb3716d729fed8d5fccaef1f500a8158da712
bbaktaeho <lth9036@gmail.com>
2021-03-07 15:45:11 +0900

    Update: 동아리 생성 메서드 추가

--- src/domain/repository/IClubRepository.ts
+++ src/domain/repository/IClubRepository.ts
@@ -4,10 +4,12 @@ import { Club } from '../entity/Club';
  * todo: 추천 (새내기 추천, 현내기 추천)
  */
 export interface IClubRepository {
-  getClubs(): Promise<Club[]>;
+  createClub(club: Club): Promise<Club>;
+  getClubById(clubId: number): Promise<Club>;
+  getAllClubs(): Promise<Club[]>;
   getClubsByCategory(categoryId: number): Promise<Club[]>;
   getClubsByKeyword(keyword: string): Promise<Club[]>;
-  updateClub(club: Club): Promise<Club>;
-  removeClub(clubId: number): Promise<any>;
+  updateClub(club: Club): Promise<void>;
+  removeClub(clubId: number): Promise<void>;
   recommendClubs(): Promise<any>;
 }
