aea04efed40616883152a0f6a1fb01b463b42c01
bbaktaeho <lth9036@gmail.com>
2021-03-09 18:05:15 +0900

    Update: Swagger 설정

--- src/common/swagger/SwaggerConfig.ts
+++ src/common/swagger/SwaggerConfig.ts
@@ -4,7 +4,7 @@ import { SWAGGER_TAGS } from './SwaggerTags';
 export const SWAGGER_CONFIG_v1: ISwaggerConfig = {
   title: 'INU CLUB API',
   description: '인천대학교 동아리 애플리케이션 API',
-  version: '1.0',
+  version: '1.0.0',
   tags: SWAGGER_TAGS,
   basePath: '/api/v1',
 };
