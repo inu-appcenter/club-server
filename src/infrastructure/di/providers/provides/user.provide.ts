13542434d7e42760301d4b5c15181f83a9a098b6
bbaktaeho <lth9036@gmail.com>
2021-03-17 16:51:37 +0900

    Add: Club Today Providers

--- src/infrastructure/di/providers/provides/user.provide.ts
+++ src/infrastructure/di/providers/provides/user.provide.ts
@@ -1,4 +1,5 @@
 const GET_USER_PROXY_SERVICE = 'getUserProxyService';
+const GET_USER_LIST_PROXY_SERVICE = 'getUserListProxyService';
 const CREATE_USER_PROXY_SERVICE = 'createUserProxyService';
 const UPDATE_USER_PROXY_SERVICE = 'updateUserProxyService';
 const REMOVE_USER_PROXY_SERVICE = 'removeUserProxyService';
@@ -6,6 +7,7 @@ const REQUEST_ADMIN_PROXY_SERVICE = 'requestAdminProxyService';
 
 export const UserProvides = {
   GET_USER_PROXY_SERVICE,
+  GET_USER_LIST_PROXY_SERVICE,
   CREATE_USER_PROXY_SERVICE,
   UPDATE_USER_PROXY_SERVICE,
   REMOVE_USER_PROXY_SERVICE,
--- src/infrastructure/di/providers/user.providers.ts
+++ src/infrastructure/di/providers/user.providers.ts
@@ -1,9 +1,10 @@
 import { UseCaseProxy } from '@/common/usecase/UseCaseProxy';
-import { CreateUser } from '@/domain/usecase/user/CreateUser';
-import { GetUser } from '@/domain/usecase/user/GetUser';
-import { RemoveUser } from '@/domain/usecase/user/RemoveUser';
-import { RequestAdmin } from '@/domain/usecase/user/RequestAdmin';
-import { UpdateUser } from '@/domain/usecase/user/UpdateUser';
+import { CreateUserUseCase } from '@/domain/usecase/user/CreateUserUseCase';
+import { GetUserListUseCase } from '@/domain/usecase/user/GetUserListUseCase';
+import { GetUserUseCase } from '@/domain/usecase/user/GetUserUseCase';
+import { RemoveUserUseCase } from '@/domain/usecase/user/RemoveUserUseCase';
+import { RequestAdminUseCase } from '@/domain/usecase/user/RequestAdminUseCase';
+import { UpdateUserUseCase } from '@/domain/usecase/user/UpdateUserUseCase';
 import { Provider } from '@nestjs/common';
 import { UserRepository } from '../../repositories/user.repository';
 import { UserProvides } from './provides/user.provide';
@@ -11,35 +12,42 @@ import { UserProvides } from './provides/user.provide';
 const GetUserProvider: Provider = {
   inject: [UserRepository],
   provide: UserProvides.GET_USER_PROXY_SERVICE,
-  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new GetUser(userRepository)),
+  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new GetUserUseCase(userRepository)),
+};
+
+const GetUserListProvider: Provider = {
+  inject: [UserRepository],
+  provide: UserProvides.GET_USER_LIST_PROXY_SERVICE,
+  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new GetUserListUseCase(userRepository)),
 };
 
 const CreateUserProvider: Provider = {
   inject: [UserRepository],
   provide: UserProvides.CREATE_USER_PROXY_SERVICE,
-  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new CreateUser(userRepository)),
+  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new CreateUserUseCase(userRepository)),
 };
 
 const UpdateUserProvider: Provider = {
   inject: [UserRepository],
   provide: UserProvides.UPDATE_USER_PROXY_SERVICE,
-  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new UpdateUser(userRepository)),
+  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new UpdateUserUseCase(userRepository)),
 };
 
 const RemoveUserProvider: Provider = {
   inject: [UserRepository],
   provide: UserProvides.REMOVE_USER_PROXY_SERVICE,
-  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new RemoveUser(userRepository)),
+  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new RemoveUserUseCase(userRepository)),
 };
 
 const RequestUserProvider: Provider = {
   inject: [UserRepository],
   provide: UserProvides.REQUEST_ADMIN_PROXY_SERVICE,
-  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new RequestAdmin(userRepository)),
+  useFactory: (userRepository: UserRepository) => new UseCaseProxy(new RequestAdminUseCase(userRepository)),
 };
 
 export const UserProviders = [
   GetUserProvider,
+  GetUserListProvider,
   CreateUserProvider,
   UpdateUserProvider,
   RemoveUserProvider,
