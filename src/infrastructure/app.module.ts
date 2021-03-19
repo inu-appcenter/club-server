366b36f151324fb4988ff009e9601953c05ab25b
bbaktaeho <lth9036@gmail.com>
2021-03-19 17:03:29 +0900

    Add: ServeStaticModule 임포트

--- src/infrastructure/app.module.ts
+++ src/infrastructure/app.module.ts
@@ -1,5 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { LoggerMiddleware } from './api/middlewares/logger.middleware';
 import { RestModule } from './api/rest/rest.module';
 import { EnvironmentConfigModule } from './config/environment/env.module';
@@ -8,6 +10,9 @@ import { EnvironmentConfigModule } from './config/environment/env.module';
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), // .env 설정
     EnvironmentConfigModule, // 환경 변수 설정
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'uploads'),
+    }),
     RestModule, // api
   ],
 })
