f56efb132852382809ac7bbc6660c664c36cf8bc
bbaktaeho <lth9036@gmail.com>
2021-03-20 18:43:28 +0900

    Update: 클럽투데이 DTO 수정

--- src/infrastructure/api/rest/clubtoday/models/dto/clubtoday.dto.ts
+++ src/infrastructure/api/rest/clubtoday/models/dto/clubtoday.dto.ts
@@ -6,6 +6,10 @@ export class ClubTodayDTO {
   @IsString()
   headerImage: string;
 
+  @ApiProperty({ type: String, description: '제목', example: '앱센터 13.5기 모집' })
+  @IsString()
+  title: string;
+
   @ApiProperty({ type: String, description: 'html 코드', example: '<h1>프론트랑 논의가 필요합니다</h1>' })
   @IsString()
   body: string;
