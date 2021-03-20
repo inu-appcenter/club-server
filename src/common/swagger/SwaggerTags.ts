b833c033fa667daff6c2a59a943f350dd72f5652
bbaktaeho <lth9036@gmail.com>
2021-03-20 18:36:30 +0900

    Update: 카테고리 태그 추가

--- src/common/swagger/SwaggerTags.ts
+++ src/common/swagger/SwaggerTags.ts
@@ -3,7 +3,7 @@ import { ISwaggerTag } from './ISwaggerConfig';
 export const SWAGGER_TAG_AUTH: ISwaggerTag = { tag: 'Auth', desc: '사용자 인증 API' };
 export const SWAGGER_TAG_USER: ISwaggerTag = { tag: 'User', desc: '사용자 API' };
 export const SWAGGER_TAG_ADMIN: ISwaggerTag = { tag: 'Admin', desc: '관리자 API' };
-export const SWAGGER_TAG_SUPERADMIN: ISwaggerTag = { tag: 'SuperAdmin', desc: '총 관리자 API' };
+export const SWAGGER_TAG_SUPERADMIN: ISwaggerTag = { tag: 'SuperAdmin', desc: '슈퍼관리자 API' };
 export const SWAGGER_TAG_CLUB: ISwaggerTag = { tag: 'Club', desc: '동아리 API' };
 export const SWAGGER_TAG_CLUBTODAY: ISwaggerTag = { tag: 'Club-ClubToday', desc: '클럽투데이 API' };
 export const SWAGGER_TAG_CLUB_SEARCH: ISwaggerTag = { tag: 'Club-Search', desc: '동아리 검색 API' };
@@ -16,7 +16,7 @@ export const SWAGGER_TAG_GATHERING_PARTICIPATION: ISwaggerTag = {
   desc: '소모임 참가 API',
 };
 export const SWAGGER_TAG_UPLOAD_IMAGE: ISwaggerTag = { tag: 'Upload-Image', desc: '사진 업로드 API' };
-// export const SWAGGER_TAG_CATEGORY = { tag: 'Category', desc: '카테고리 API' };
+export const SWAGGER_TAG_CATEGORY = { tag: 'Category', desc: '카테고리 API (슈퍼관리자 전용)' };
 
 export const SWAGGER_TAGS = [
   SWAGGER_TAG_AUTH,
@@ -32,4 +32,5 @@ export const SWAGGER_TAGS = [
   SWAGGER_TAG_GATHERING_RECOMMENT,
   SWAGGER_TAG_GATHERING_PARTICIPATION,
   SWAGGER_TAG_UPLOAD_IMAGE,
+  SWAGGER_TAG_CATEGORY,
 ];
