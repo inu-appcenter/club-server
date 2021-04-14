93587d0843d5cd5045493e51cddfa4ff95348a4e
bbaktaeho <lth9036@gmail.com>
2021-04-14 11:28:16 +0900

    Update: 예외 타입별로 다르게오는 객체를 통일

--- src/infrastructure/api/exception-filter/nest-http-exception.filter.ts
+++ src/infrastructure/api/exception-filter/nest-http-exception.filter.ts
@@ -1,29 +1,45 @@
 import { Exception } from '@/common/exception/Exception';
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { IErrorResponse } from '@/common/exception/IErrorResponse';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+/**
+ * 모든 예외를 캐치
+ */
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    if (exception instanceof Exception) {
-      const errors = exception.data.errors;
-      if (errors) {
-        // todo
-      }
-      response.status(400).json({
-        timestamp: new Date().toISOString(),
-      });
-    } else {
-      const status = exception.getStatus();
 
-      response.status(status).json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-      });
+    const errorRes: IErrorResponse = {
+      errors: [],
+      path: '',
+      timestamp: new Date().toISOString(),
+    };
+    let status: number;
+    // Common Exception 타입의 에러일 때
+    if (exception instanceof Exception) {
+      const data = exception.data;
+      if (data.errors) errorRes.errors = data.errors.map((e) => e.message).flat();
+      else errorRes.errors = [exception.message];
+      if (exception.code >= 600) status = 400;
+      else status = exception.code;
     }
+    // Nest HttpException 타입의 에러일 때
+    else if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      errorRes.errors = [exception.message];
+    }
+    // Error 타입의 에러일 때
+    else {
+      status = 500;
+      errorRes.errors = [exception.message];
+    }
+    // 최종 에러 응답 객체
+    errorRes.path = request.path;
+    response.status(status).json(errorRes);
+    Logger.error(`[${errorRes.timestamp}][${request.ip}] ${errorRes.errors}`);
   }
 }
