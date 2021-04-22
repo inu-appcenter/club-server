6f6dcac1700f0dc67a57ac84c01d8f8681ae891e
bbaktaeho <lth9036@gmail.com>
2021-04-23 01:30:48 +0900

    Add: api부터 구현하면 됩니다!

--- src/infrastructure/di/providers/clubtoday.providers.ts
+++ src/infrastructure/di/providers/clubtoday.providers.ts
@@ -1,59 +1,62 @@
-// import { UseCaseProxy } from '@/common/usecase/UseCaseProxy';
-// import { DeleteClubTodayUseCase } from '@/domain/usecase/clubtoday/DeleteClubTodayUseCase';
-// import { GetClubTodayListByClubUseCase } from '@/domain/usecase/clubtoday/GetClubTodayListByClubUseCase';
-// import { GetClubTodayListUseCase } from '@/domain/usecase/clubtoday/GetClubTodayListUseCase';
-// import { GetClubTodayUseCase } from '@/domain/usecase/clubtoday/GetClubTodayUseCase';
-// import { ClubTodayRepository } from '@/infrastructure/repositories/clubtoday.repository';
-// import { Provider } from '@nestjs/common';
-// import { ClubTodayProvides } from './provides/clubtoday.provide';
+import { UseCaseProxy } from '@/common/usecase/UseCaseProxy';
+import { CreateClubTodayUseCase } from '@/domain/usecase/clubtoday/CreateClubTodayUseCase';
+import { DeleteClubTodayUseCase } from '@/domain/usecase/clubtoday/DeleteClubTodayUseCase';
+import { GetClubTodayListByClubUseCase } from '@/domain/usecase/clubtoday/GetClubTodayListByClubUseCase';
+import { GetClubTodayListUseCase } from '@/domain/usecase/clubtoday/GetClubTodayListUseCase';
+import { GetClubTodayUseCase } from '@/domain/usecase/clubtoday/GetClubTodayUseCase';
+import { UpdateClubTodayUseCase } from '@/domain/usecase/clubtoday/UpdateClubTodayUseCase';
+import { ClubRepository } from '@/infrastructure/repositories/club.repository';
+import { ClubTodayRepository } from '@/infrastructure/repositories/clubtoday.repository';
+import { Provider } from '@nestjs/common';
+import { ClubTodayProvides } from './provides/clubtoday.provide';
 
-// const GetClubTodayProvider: Provider = {
-//   inject: [ClubTodayRepository],
-//   provide: ClubTodayProvides.GET_CLUB_TODAY_PROXY_SERVICE,
-//   useFactory: (clubTodayRepository: ClubTodayRepository) =>
-//     new UseCaseProxy(new GetClubTodayUseCase(clubTodayRepository)),
-// };
+const GetClubTodayProvider: Provider = {
+  inject: [ClubTodayRepository],
+  provide: ClubTodayProvides.GET_CLUB_TODAY_PROXY_SERVICE,
+  useFactory: (clubTodayRepository: ClubTodayRepository) =>
+    new UseCaseProxy(new GetClubTodayUseCase(clubTodayRepository)),
+};
 
-// const GetClubTodayListProvider: Provider = {
-//   inject: [ClubTodayRepository],
-//   provide: ClubTodayProvides.GET_CLUB_TODAY_LIST_PROXY_SERVICE,
-//   useFactory: (clubTodayRepository: ClubTodayRepository) =>
-//     new UseCaseProxy(new GetClubTodayListUseCase(clubTodayRepository)),
-// };
+const GetClubTodayListProvider: Provider = {
+  inject: [ClubTodayRepository],
+  provide: ClubTodayProvides.GET_CLUB_TODAY_LIST_PROXY_SERVICE,
+  useFactory: (clubTodayRepository: ClubTodayRepository) =>
+    new UseCaseProxy(new GetClubTodayListUseCase(clubTodayRepository)),
+};
 
-// const GetClubTodayListByClubProvider: Provider = {
-//   inject: [ClubTodayRepository],
-//   provide: ClubTodayProvides.GET_CLUB_TODAY_LIST_BY_CLUB_PROXY_SERVICE,
-//   useFactory: (clubTodayRepository: ClubTodayRepository) =>
-//     new UseCaseProxy(new GetClubTodayListByClubUseCase(clubTodayRepository)),
-// };
+const GetClubTodayListByClubProvider: Provider = {
+  inject: [ClubTodayRepository],
+  provide: ClubTodayProvides.GET_CLUB_TODAY_LIST_BY_CLUB_PROXY_SERVICE,
+  useFactory: (clubTodayRepository: ClubTodayRepository) =>
+    new UseCaseProxy(new GetClubTodayListByClubUseCase(clubTodayRepository)),
+};
 
-// // const CreateClubTodayProvider: Provider = {
-// //   inject: [ClubTodayRepository],
-// //   provide: ClubTodayProvides.CREATE_CLUB_TODAY_PROXY_SERVICE,
-// //   useFactory: (clubTodayRepository: ClubTodayRepository) =>
-// //     new UseCaseProxy(new CreateClubTodayUseCase(clubTodayRepository)),
-// // };
+const CreateClubTodayProvider: Provider = {
+  inject: [ClubTodayRepository, ClubRepository],
+  provide: ClubTodayProvides.CREATE_CLUB_TODAY_PROXY_SERVICE,
+  useFactory: (clubTodayRepository: ClubTodayRepository, clubRepository: ClubRepository) =>
+    new UseCaseProxy(new CreateClubTodayUseCase(clubTodayRepository, clubRepository)),
+};
 
-// // const UpdateClubTodayProvider: Provider = {
-// //   inject: [ClubTodayRepository],
-// //   provide: ClubTodayProvides.UPDATE_CLUB_TODAY_PROXY_SERVICE,
-// //   useFactory: (clubTodayRepository: ClubTodayRepository) =>
-// //     new UseCaseProxy(new UpdateClubTodayUseCase(clubTodayRepository)),
-// // };
+const UpdateClubTodayProvider: Provider = {
+  inject: [ClubTodayRepository],
+  provide: ClubTodayProvides.UPDATE_CLUB_TODAY_PROXY_SERVICE,
+  useFactory: (clubTodayRepository: ClubTodayRepository) =>
+    new UseCaseProxy(new UpdateClubTodayUseCase(clubTodayRepository)),
+};
 
-// const RemoveClubTodayProvider: Provider = {
-//   inject: [ClubTodayRepository],
-//   provide: ClubTodayProvides.REMOVE_CLUB_TODAY_PROXY_SERVICE,
-//   useFactory: (clubTodayRepository: ClubTodayRepository) =>
-//     new UseCaseProxy(new DeleteClubTodayUseCase(clubTodayRepository)),
-// };
+const RemoveClubTodayProvider: Provider = {
+  inject: [ClubTodayRepository],
+  provide: ClubTodayProvides.REMOVE_CLUB_TODAY_PROXY_SERVICE,
+  useFactory: (clubTodayRepository: ClubTodayRepository) =>
+    new UseCaseProxy(new DeleteClubTodayUseCase(clubTodayRepository)),
+};
 
-// export const ClubTodayProviders = [
-//   GetClubTodayProvider,
-//   GetClubTodayListProvider,
-//   GetClubTodayListByClubProvider,
-//   //   CreateClubTodayProvider,
-//   //   UpdateClubTodayProvider,
-//   RemoveClubTodayProvider,
-// ];
+export const ClubTodayProviders = [
+  GetClubTodayProvider,
+  GetClubTodayListProvider,
+  GetClubTodayListByClubProvider,
+  CreateClubTodayProvider,
+  UpdateClubTodayProvider,
+  RemoveClubTodayProvider,
+];
--- src/infrastructure/repositories/clubtoday.repository.ts
+++ src/infrastructure/repositories/clubtoday.repository.ts
@@ -2,7 +2,7 @@ import { ClubToday } from '@/domain/entity/ClubToday';
 import { IClubTodayRepository } from '@/domain/repository/IClubTodayRepository';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { createQueryBuilder, getConnection, Repository } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
 import { toClubToday } from './converters/clubtoday.converter';
 import { OrmCategory } from './entities/category.entity';
 import { OrmClub } from './entities/club.entity';
@@ -41,11 +41,15 @@ export class ClubTodayRepository implements IClubTodayRepository {
       .getOne();
     throw new Error('기달');
   }
-  getClubTodayListByClubId(clubId: number): Promise<ClubToday[]> {
-    throw new Error('Method not implemented.');
+
+  async getClubTodayListByClubId(clubId: number): Promise<ClubToday[]> {
+    const ormClub = await this.ormClubRepository.findOne(clubId, { relations: ['clubTodays'] });
+    return await Promise.all(ormClub.clubTodays.map((orm) => toClubToday(orm)));
   }
-  getClubTodayById(clubTodayId: number): Promise<ClubToday> {
-    throw new Error('Method not implemented.');
+
+  async getClubTodayById(clubTodayId: number): Promise<ClubToday> {
+    const ormClubToday = await this.ormClubTodayRepository.findOne(clubTodayId);
+    return await toClubToday(ormClubToday);
   }
 
   async createClubToday(clubToday: ClubToday): Promise<ClubToday> {
@@ -54,10 +58,12 @@ export class ClubTodayRepository implements IClubTodayRepository {
     return toClubToday(ormClubToday);
   }
 
-  updateClubToday(clubToday: ClubToday): Promise<void> {
-    throw new Error('Method not implemented.');
+  async updateClubToday(clubToday: ClubToday): Promise<void> {
+    const ormClubToday = await this.toOrmClubToday(clubToday);
+    await this.ormClubTodayRepository.save(ormClubToday);
   }
-  deleteClubTodayById(clubTodayId: number): Promise<void> {
-    throw new Error('Method not implemented.');
+
+  async deleteClubTodayById(clubTodayId: number): Promise<void> {
+    await this.ormClubTodayRepository.delete({ id: clubTodayId });
   }
 }
